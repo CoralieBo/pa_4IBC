dae996a16473b1ecfe0aeba3e2688cdc3706eeda
Coralie <coralie@digitalcube.net>
2024-02-21 11:03:37 +0100

    create 404 error page

--- pouswapfront/src/App.tsx
+++ pouswapfront/src/App.tsx
@@ -1,13 +1,14 @@
 import './styles/app.scss';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/404/404';
 
 function App() {
   return (
     <div className='min-h-screen bg'>
       <Header />
       <Routes>
-        <Route path='*' element={<div>404</div>} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
--- pouswapfront/tailwind.config.js
+++ pouswapfront/tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        color: {'logo': '#A87A51', "primary": "rgb(156, 189, 231)", "secondary": "rgb(253, 240, 151)"},
         colors: {green1: "#658864", green2: "#B7B78A", white1: "#fdf6fd", orange: "#bc6c25", yellow: "#ecd79b", gray1: "#DDDDDD", white2: "#EEEEEE", gray2: "#d1d1d1", black1: "#292524", black2: "#78716c"},
-        background: {primary: 'rgb(247,247,247)', secondary: '#FFFFFF'},
       }
     },
   },
