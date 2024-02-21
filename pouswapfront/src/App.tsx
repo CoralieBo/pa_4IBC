d5ceb89c5969ed0a8ea625e4d950fc3ead85f1c7
Coralie <coralie@digitalcube.net>
2024-02-21 11:05:17 +0100

    create home page

--- pouswapfront/src/App.tsx
+++ pouswapfront/src/App.tsx
@@ -2,12 +2,14 @@ import './styles/app.scss';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './components/404/404';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className='min-h-screen bg'>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
