fea0771f92d18e0ec8ecaa22ab4cb017a4acd0c0
Coralie <coralie@digitalcube.net>
2024-02-21 11:30:53 +0100

    create swap page

--- pouswapfront/src/App.tsx
+++ pouswapfront/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './components/404/404';
 import Home from './components/Home/Home';
+import Swap from './components/Swap/Swap';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/swap" element={<Swap />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
