1e2df09fc86f1761af0c91d3f4d8705f99e95de3
coralieBo <coralieboyer@Coralies-MacBook-Pro.local>
2024-04-06 17:01:29 +0200

    add field in user table

--- pouswapback/models/user.go
+++ pouswapback/models/user.go
@@ -5,5 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	PublicKey string `json:"public_key"`
-	Role      string `json:"role"`
+	Role      string `json:"role"`   // USER / ADMIN
+	Status    string `json:"status"` // ACTIVE / BAN
 }
